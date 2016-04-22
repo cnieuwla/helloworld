81b4c77e8f008134e036e2b2c27308a126a243b2
Chiel Nieuwlaat <cnieuwla@avans.nl>
2016-04-22 09:06:13 +0200

    eerste keer

---
+++ src/hello/world/HelloWorld.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.world;
+
+/**
+ *
+ * @author Chiel
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String var = "Hello World2!!";
+        Integer a = 1;
+        Integer b = 2;
+        Integer c = a+b;
+        
+        System.out.println("Hello World!!");
+        System.out.println(var);
+        System.out.println(c);
+    }
+    
+}
